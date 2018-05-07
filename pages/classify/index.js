16023830518515064fdae99e7a6e5219b2edd14d
RongSen <610567970@qq.com>
2018-05-08 00:12:42 +0800

    Initial commit

---
+++ pages/classify/index.js
@@ -0,0 +1,412 @@
+// pages/classify/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    "typeId": "type_id_1",
+    "currentTypeId":"type_id_1",
+    "navLeftItemList":[
+      {
+        "id": "type_id_1",
+        "name":"宝宝奶粉",
+        "desc":""
+      },
+      {
+        "id": "type_id_2",
+        "name":"纸尿裤",
+        "desc":""
+      },
+      {
+        "id": "type_id_3",
+          "name": "纸尿裤",
+          "desc": ""
+      },
+      {
+        "id": "type_id_4",
+        "name": "纸尿裤",
+        "desc": ""
+      },
+      {
+        "id": "type_id_5",
+          "name": "纸尿裤",
+          "desc": ""
+      },
+      {
+        "id": "type_id_6",
+        "name": "纸尿裤",
+        "desc": ""
+      },
+      {
+        "id": "type_id_7",
+        "name": "纸尿裤",
+        "desc": ""
+      },
+      {
+        "id": "type_id_8",
+        "name": "纸尿裤",
+        "desc": ""
+      },
+      {
+        "id": "type_id_9",
+        "name": "纸尿裤",
+        "desc": ""
+      },
+      {
+        "id": "type_id_10",
+        "name": "纸尿裤",
+        "desc": ""
+      },
+      {
+        "id": "type_id_11",
+        "name": "纸尿裤",
+        "desc": ""
+      },
+      {
+        "id": "type_id_12",
+        "name": "纸尿裤",
+        "desc": ""
+      },
+      {
+        "id": "type_id_13",
+        "name": "纸尿裤",
+        "desc": ""
+      },
+      {
+        "id": "type_id_14",
+        "name": "纸尿裤",
+        "desc": ""
+      },
+      {
+        "id": "type_id_15",
+        "name": "纸尿裤",
+        "desc": ""
+      },
+      {
+        "id": "type_id_16",
+        "name": "纸尿裤",
+        "desc": ""
+      }
+    ],
+    "navRightItemList":[
+      {
+        "typeId":"type_id_1",
+        "typeDetail":[
+          {
+            "typeId": "type_id_1",
+            "id":"type_detail_id_1",
+            "name":"纽崔莱",
+            "logo":"/images/type/logo_50_30.jpg",
+            "desc":""
+          },
+          {
+            "typeId": "type_id_1",
+            "id": "type_detail_id_2",
+            "name": "施恩",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_1",
+            "id": "type_detail_id_3",
+            "name": "伊利",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          }
+        ]
+      },
+      {
+        "typeId": "type_id_2",
+        "typeDetail": [
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_1",
+            "name": "花王",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_2",
+            "name": "贝亲",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          },
+          {
+            "typeId": "type_id_2",
+            "id": "type_detail_id_3",
+            "name": "乐贝",
+            "logo": "/images/type/logo_50_30.jpg",
+            "desc": ""
+          }
+        ]
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  switchRightTab: function(e){
+    var typeData = e.target.dataset;
+    var typeId = typeData.id;
+    var currentTypeId = typeData.index;
+    //将原始数据保存起来
+    var initNavRightItemList = [];
+    if (!this.data.initNavRightItemList)
+      initNavRightItemList = this.data.navRightItemList;
+    else
+      initNavRightItemList = this.data.initNavRightItemList;
+    //根据当前类型获取想要类型明细
+    var navRightItemList = [];
+    for(var i=0;i<initNavRightItemList.length;i++){
+      var data = initNavRightItemList[i];
+      if(typeId==data.typeId)
+        navRightItemList.push(initNavRightItemList[i]);
+    }
+    //重置页面数据
+    this.setData({
+      "typeId": typeId,
+      "currentTypeId": currentTypeId,
+      "navRightItemList": navRightItemList,
+      "initNavRightItemList": initNavRightItemList
+    });
+    this.onLoad(this);
+  },
+  gotoGoodsList: function(e){
+    var typeDetail = e.currentTarget.dataset.typedetail;
+    var typeId = typeDetail.typeId;
+    var typeDetailId = typeDetail.id;
+    var name = typeDetail.name;
+    var desc = typeDetail.desc;
+    var logo = typeDetail.logo;
+
+    wx.navigateTo({
+      url: '../list/index?typeId='+typeId+'&typeDetailId='+typeDetailId,
+      success: function(){
+
+      },
+      fail: function(){},
+      complete: function(){}
+    })
+  }
+})
---
+++ pages/detail/index.js
@@ -0,0 +1,99 @@
+// pages/detail/index.wxml.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 1200
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+
+    wx.request({
+      url: 'http://huanqiuxiaozhen.com/wemall/goods/inqgoods?id='+options.id,
+      method: 'GET',
+      header: {
+        "Accept": "application/json"
+      },
+      success: function(res){
+        var shoppingDetails = res.data.data;
+
+        console.log(shoppingDetails);
+
+        var goodsPicsInfo = [];
+        var goodsPicsObj = {};
+        var goodspic = res.data.data.goodspics;
+        var goodspics = goodspic.substring(0, goodspic.length-1);
+
+        var goodspicsArr = goodspics.split("#");
+        for(var i = 0; i < goodspicsArr.length; i++){
+          goodsPicsInfo.push({
+            "picurl": goodspicsArr[i]
+          });
+        }
+        that.setData({
+          goodsPicsInfo: goodsPicsInfo,
+          shoppingDetails: shoppingDetails
+        });
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
